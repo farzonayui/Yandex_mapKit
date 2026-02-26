105310d2f632b9af486a47cb8f8cc601932c6737
farzonayui <farzonakhon2018@gmail.com>
2026-02-26 21:56:59 +0500

    Initial Commit

--- app/src/main/java/com/example/map/MainActivity.kt
+++ app/src/main/java/com/example/map/MainActivity.kt
@@ -1,40 +1,43 @@
 package com.example.map
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
-import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.MapObjectTapListener
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.runtime.image.ImageProvider
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var mapView: MapView
 
+    private val placemarkTapListener = MapObjectTapListener { _, point ->
+        Toast.makeText(
+            this@MainActivity,
+            "Tapped the point (${point.longitude}, ${point.latitude})",
+            Toast.LENGTH_SHORT
+        ).show()
+        true
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
-
 
         MapKitFactory.setApiKey("81d5d669-40cd-4f44-80c5-9ec755bad873")
         MapKitFactory.initialize(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         mapView = findViewById(R.id.mapview)
 
+        val imageProvider = ImageProvider.fromResource(this, R.drawable.location)
+        val placemark = mapView.map.mapObjects.addPlacemark().apply {
+            geometry = Point(40.281919, 69.617063)
+            setIcon(imageProvider)
+        }
 
-        val map = mapView.mapWindow.map
-        map.move(
-            CameraPosition(
-                Point(40.281919, 69.617063),
-                17.0f,
-                150.0f,
-                30.0f
-            )
-        )
+        placemark.addTapListener(placemarkTapListener)
     }
-
-
     override fun onStart() {
         super.onStart()
         MapKitFactory.getInstance().onStart()
