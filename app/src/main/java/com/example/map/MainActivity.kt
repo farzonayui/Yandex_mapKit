12cff4bf16396326ecef8325af57fcbd05764644
farzonayui <farzonakhon2018@gmail.com>
2026-03-04 20:55:09 +0500

    Initial Commit

--- app/src/main/java/com/example/map/MainActivity.kt
+++ app/src/main/java/com/example/map/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.map
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
-import com.yandex.mapkit.map.MapObjectTapListener
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.InputListener
+import com.yandex.mapkit.map.Map
 import com.yandex.mapkit.mapview.MapView
 import com.yandex.runtime.image.ImageProvider
 
@@ -13,14 +14,6 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var mapView: MapView
 
-    private val placemarkTapListener = MapObjectTapListener { _, point ->
-        Toast.makeText(
-            this@MainActivity,
-            "Tapped the point (${point.longitude}, ${point.latitude})",
-            Toast.LENGTH_SHORT
-        ).show()
-        true
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
 
         MapKitFactory.setApiKey("81d5d669-40cd-4f44-80c5-9ec755bad873")
@@ -30,14 +23,42 @@ class MainActivity : ComponentActivity() {
 
         mapView = findViewById(R.id.mapview)
 
-        val imageProvider = ImageProvider.fromResource(this, R.drawable.location)
-        val placemark = mapView.map.mapObjects.addPlacemark().apply {
-            geometry = Point(40.281919, 69.617063)
-            setIcon(imageProvider)
+        val startPoint = Point(40.281919, 69.617063)
+        mapView.map.move(
+            CameraPosition(
+                startPoint,
+                17.0f,
+                150.0f,
+                30.0f
+            )
+        )
+        val imageProvider =
+            ImageProvider.fromResource(this, R.drawable.location)
+        val pointIcon =
+            ImageProvider.fromResource(this, R.drawable.points)
+
+        mapView.map.mapObjects.addPlacemark(startPoint).apply {
+            setIcon(pointIcon)
+
         }
 
-        placemark.addTapListener(placemarkTapListener)
+
+        mapView.map.addInputListener(object : InputListener {
+
+            override fun onMapTap(map: Map, point: Point) {
+
+                map.mapObjects.clear()
+
+                map.mapObjects.addPlacemark(point).apply {
+                    setIcon(imageProvider)
+                }
+            }
+
+            override fun onMapLongTap(map: Map, point: Point) {
+            }
+        })
     }
+
     override fun onStart() {
         super.onStart()
         MapKitFactory.getInstance().onStart()
